1d60affb4bba7ad1fe7780a809c2d75c8af2c0c2
Mauro Gioberti <maurol.gioberti@gmail.com>
2024-02-08 09:57:24 +0100

    Fix class tag name

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = () => (
   <div>
-    <h2 class="text-center mb-4">About Us</h2>
+    <h2 className="text-center mb-4">About Us</h2>
     <p>SPOT stands at the forefront of online IT education, committed to delivering high-quality and accessible learning experiences for everyone. Utilizing Bootstrap 5, our platform offers a responsive, mobile-first design that enhances user engagement across all devices.</p>
     <p>Our courses showcase dynamic content retrieval using simulated backend interactions, demonstrating our ability to present informative and engaging material without real backend connectivity. We adhere to React best practices, ensuring a scalable, efficient, and maintainable platform.</p>
     <p>Comprehensive testing strategies, including unit and integration tests, underpin SPOT's reliability. Our dedication to extensive code coverage ensures a robust learning environment, free from disruptions. At SPOT, we empower learners worldwide by providing accessible, top-tier IT education, setting new standards in online learning.</p>
